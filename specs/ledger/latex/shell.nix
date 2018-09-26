7fe5fe8982f665364825091ab64e71905c745728
Damian Nadales <damian.nadales@iohk.io>
2018-09-26 08:58:11 +0200

    Use the `mathpazo` font [skip ci]
    This adds a fix for the nix configuration.

--- specs/ledger/latex/shell.nix
+++ specs/ledger/latex/shell.nix
@@ -6,8 +6,11 @@ stdenv.mkDerivation {
                       scheme-small
 
                       # libraries
-                      stmaryrd lm-math amsmath mathpazo
+                      stmaryrd lm-math amsmath
                       extarrows cleveref semantic
+
+                      # font libraries `mathpazo` seems to depend on palatino, but it isn't pulled.
+                      mathpazo palatino microtype
 
                       # libraries for marginal notes
                       xargs todonotes
