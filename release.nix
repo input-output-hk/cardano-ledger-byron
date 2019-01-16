6e73a5d56e62a5a26e9a20fd91a6b33d5f80ac2e
Nicholas Clarke <nick@topos.org.uk>
2019-01-16 12:51:37 +0100

    Fix: Try defining the common stuff in release.nix

--- release.nix
+++ release.nix
@@ -1,19 +1,22 @@
 { chain ? { outPath = ./.; rev = "abcdef"; } }:
 let
   pkgs = import ./pkgs.nix;
+  specLatexCommon = pkgs.copyPathToStore ./specs/common/latex;
 in pkgs.lib.fix (self: {
   forceNewEval = pkgs.writeText "forceNewEval" chain.rev;
 
   required = pkgs.lib.hydraJob (pkgs.releaseTools.aggregate {
     name = "cardano-chain-required-checks";
     constituents = with self;
-      [ byronLedgerSpec byronChainSpec semanticsSpec
+      [ specLatexCommon
+        byronLedgerSpec byronChainSpec semanticsSpec
         hsLedgerSpec hsChainSpec hsSemantics
         forceNewEval
       ];
   });
 
-  byronLedgerSpec = import ./specs/ledger/latex {};
+
+  byronLedgerSpec = import ./specs/ledger/latex {inherit specLatexCommon;};
   byronChainSpec = import ./specs/chain/latex {};
   semanticsSpec = import ./specs/semantics/latex {};
 
