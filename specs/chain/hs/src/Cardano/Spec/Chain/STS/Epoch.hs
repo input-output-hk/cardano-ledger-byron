357300115ae6ba5c2aa5809ac60cd71921dda53c
Damian Nadales <damian.nadales@iohk.io>
2019-01-24 11:05:42 +0100

    Add an EPOCH transition system.

---
+++ specs/chain/hs/src/Cardano/Spec/Chain/STS/Epoch.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Cardano.Spec.Chain.STS.Epoch where
+
+import Data.Map.Strict (Map)
+import Control.State.Transition
+
+import Ledger.Core
+
+data EPOCH
+
+instance STS EPOCH where
+  type Environment EPOCH = Map VKeyGenesis VKey
+  type State EPOCH = Epoch
+  type Signal EPOCH = Slot
+  data PredicateFailure EPOCH = X
+    deriving (Eq, Show)
+  initialRules = []
+  transitionRules =
+    [ do
+        TRC (_, _, s) <- judgmentContext
+        return $! sEpoch s
+    ]
+
+-- | Compute the epoch for the given _absolute_ slot
+sEpoch :: Slot -> Epoch
+sEpoch (Slot s) = Epoch $ s `div` slotsPerEpoch
+  where
+    -- Hardcoded number of slots per epoch, as per Byron.
+    slotsPerEpoch = 21600
