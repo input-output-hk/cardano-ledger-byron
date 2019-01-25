2d6718e1e159523ad923ea1af03c95f5df536b95
Damian Nadales <damian.nadales@iohk.io>
2019-01-25 16:21:46 +0100

    Failed attempt to build using nix.

--- pkgs.nix
+++ pkgs.nix
@@ -9,6 +9,16 @@ import nixpkgs {
           ghc861 = pkgs.haskell.packages.ghc861.override {
             overrides = self: super: {
               basement = self.callHackage "basement" "0.0.8" {};
+              nonempty-containers = self.callCabal2nix "nonempty-containers"
+                              (pkgs.fetchFromGitHub { owner = "mstksg";
+                                                      repo = "nonempty-containers";
+                                                      rev = "27fb9d78269be1b31351fff8ac24268f6a107964";
+                                                      sha256 = "1bj95siah7fv4wapzk4p28qx5yvadznc78cnfqcr3whmi1g03vna"; }) {};
+              cardano-prelude = self.callCabal2nix "cardano-prelude"
+                              (pkgs.fetchFromGitHub { owner = "input-output-hk";
+                                                      repo = "cardano-prelude";
+                                                      rev = "be0836078ca0c8e521376baf7cf9a01abdd3664b";
+                                                      sha256 = "1rdimji8a5v7wcvsc0q1g57hmlxpzm736ilcxspky3wvy55xjab0"; }) {};
               memory = pkgs.haskell.lib.doJailbreak (self.callHackage "memory" "0.14.18" {});
               small-steps = self.callCabal2nix "small-steps" ./specs/semantics/hs {};
               cs-ledger = pkgs.haskell.lib.overrideCabal (self.callCabal2nix "cs-ledger" ./specs/ledger/hs {}) (old: {
