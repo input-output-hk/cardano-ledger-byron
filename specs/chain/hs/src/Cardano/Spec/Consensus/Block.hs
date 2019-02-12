95835d6deff0af9234bf9b7b0b0ac858c136c97a
Marko Dimjašević <marko.dimjasevic@iohk.io>
2019-02-12 13:19:34 +0100

    Issue #304: minor polishing

--- specs/chain/hs/src/Cardano/Spec/Consensus/Block.hs
+++ specs/chain/hs/src/Cardano/Spec/Consensus/Block.hs
@@ -15,20 +15,20 @@ import           Ledger.Signatures
 class BlockHeader h where
   -- | Hash of the previous block header, or 'genesisHash' in case of
   -- the first block in a chain.
-  bhPrevHash :: h -> Hash
+  bhPrevHash :: h -> Hash -- This is needed in the PBFT rule
   -- | Header hash
   bhHash :: h -> Hash
   -- | Signature of the block by its issuer.
-  bSig :: h -> Sig VKey
+  bhSig :: h -> Sig VKey
   -- | Block issuer.
-  bIssuer :: h -> VKey
+  bhIssuer :: h -> VKey
   -- | Slot for which this block is issued
   bhSlot :: h -> Slot
 
 
 class BlockBody bb where
   -- | Delegation certificates.
-  bCerts :: bb -> [DCert]
+  bbCerts :: bb -> [DCert]
 
 
 class ( BlockHeader (FamBlockHeader b)
@@ -44,15 +44,15 @@ class ( BlockHeader (FamBlockHeader b)
 
 
 instance BlockBody CBM.BlockBody where
-  bCerts (CBM.BlockBody bDCerts) = bDCerts
+  bbCerts (CBM.BlockBody bDCerts) = bDCerts
 
 instance BlockHeader CBM.BlockHeader where
   bhPrevHash h = h ^. CBM.prevHHash
    -- TODO: a corresponding field as of Feb 11, 2019 does not exist in
    -- the CBM.BlockHeader type
   bhHash       = undefined
-  bSig       h = h ^. CBM.bSig
-  bIssuer    h = h ^. CBM.bIssuer
+  bhSig      h = h ^. CBM.bSig
+  bhIssuer   h = h ^. CBM.bIssuer
   bhSlot     h = h ^. CBM.bSlot
 
 instance Block CBM.Block where
