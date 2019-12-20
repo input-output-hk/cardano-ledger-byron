cf2717faa905659a915382a37b0b8b31dac21f02
Duncan Coutts <duncan@well-typed.com>
2019-12-20 18:44:00 +0100

    Remove unused code from Cardano.Chain.Genesis.NonAvvmBalances
    Legacy from cardano-sl that is no longer used anywhere.

--- cardano-ledger/src/Cardano/Chain/Genesis/NonAvvmBalances.hs
+++ cardano-ledger/src/Cardano/Chain/Genesis/NonAvvmBalances.hs
@@ -10,26 +10,19 @@
 
 module Cardano.Chain.Genesis.NonAvvmBalances
   ( GenesisNonAvvmBalances(..)
-  , convertNonAvvmDataToBalances
   )
 where
 
 import Cardano.Prelude
 
 import Control.Monad.Except (MonadError(..))
-import qualified Data.Map.Strict as M
-import Formatting (bprint, build)
+import Formatting (bprint)
 import qualified Formatting.Buildable as B
 import Text.JSON.Canonical (FromJSON(..), ToJSON(..))
 
-import Cardano.Binary (DecoderError)
 import Cardano.Chain.Common
   ( Address
   , Lovelace
-  , LovelaceError
-  , addLovelace
-  , decodeAddressBase58
-  , integerToLovelace
   )
 
 
@@ -51,45 +44,3 @@ instance Monad m => ToJSON m GenesisNonAvvmBalances where
 instance MonadError SchemaError m => FromJSON m GenesisNonAvvmBalances where
   fromJSON = fmap GenesisNonAvvmBalances . fromJSON
 
-data NonAvvmBalancesError
-  = NonAvvmBalancesLovelaceError LovelaceError
-  | NonAvvmBalancesDecoderError DecoderError
-
-instance B.Buildable NonAvvmBalancesError where
-  build = \case
-    NonAvvmBalancesLovelaceError err -> bprint
-      ("Failed to construct a lovelace in NonAvvmBalances.\n Error: " . build)
-      err
-    NonAvvmBalancesDecoderError err -> bprint
-      ("Failed to decode NonAvvmBalances.\n Error: " . build)
-      err
-
--- | Generate genesis address distribution out of avvm parameters. Txdistr of
---   the utxo is all empty. Redelegate it in calling function.
-convertNonAvvmDataToBalances
-  :: forall m
-   . MonadError NonAvvmBalancesError m
-  => Map Text Integer
-  -> m GenesisNonAvvmBalances
-convertNonAvvmDataToBalances balances = fmap GenesisNonAvvmBalances $ do
-  converted <- traverse convert (M.toList balances)
-  mkBalances converted
- where
-  mkBalances :: [(Address, Lovelace)] -> m (Map Address Lovelace)
-  mkBalances =
-    -- Pull 'LovelaceError's out of the 'Map' and lift them to
-    -- 'NonAvvmBalancesError's
-    (`wrapError` NonAvvmBalancesLovelaceError)
-    . sequence
-    -- Make map joining duplicate keys with 'addLovelace' lifted from 'Lovelace ->
-    -- Lovelace -> Either LovelaceError Lovelace' to 'Either LovelaceError Lovelace -> Either
-    -- LovelaceError Lovelace -> Either LovelaceError Lovelace'
-    . M.fromListWith (\c -> join . liftM2 addLovelace c)
-    -- Lift the 'Lovelace's to 'Either LovelaceError Lovelace's
-    . fmap (second Right)
-
-  convert :: (Text, Integer) -> m (Address, Lovelace)
-  convert (txt, i) = do
-    addr <- decodeAddressBase58 txt `wrapError` NonAvvmBalancesDecoderError
-    lovelace <-integerToLovelace i `wrapError` NonAvvmBalancesLovelaceError
-    return (addr, lovelace)
