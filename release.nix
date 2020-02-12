e314f6ef84cd730dc0f743aa191bd1c3343e88ef
Samuel Leathers <samuel.leathers@iohk.io>
2020-02-12 11:05:01 -0500

    fix release.nix

--- release.nix
+++ release.nix
@@ -8,12 +8,12 @@
 ############################################################################
 
 # The project sources
-{ cardano-node ? { outPath = ./.; rev = "abcdef"; }
+{ cardano-ledger ? { outPath = ./.; rev = "abcdef"; }
 
 # Function arguments to pass to the project
 , projectArgs ? {
     config = { allowUnfree = false; inHydra = true; };
-    gitrev = cardano-node.rev;
+    gitrev = cardano-ledger.rev;
   }
 
 # The systems that the jobset will be built for.
@@ -36,59 +36,13 @@
 with (import pkgs.iohkNix.release-lib) {
   inherit pkgs;
   inherit supportedSystems supportedCrossSystems scrubJobs projectArgs;
-  packageSet = import cardano-node;
-  gitrev = cardano-node.rev;
+  packageSet = import cardano-ledger;
+  gitrev = cardano-ledger.rev;
 };
 
 with pkgs.lib;
 
 let
-  nixosTests = (import ./. {}).nixosTests;
-  getArchDefault = system: let
-    table = {
-      x86_64-linux = import ./. { system = "x86_64-linux"; };
-      x86_64-darwin = import ./. { system = "x86_64-darwin"; };
-      x86_64-windows = import ./. { system = "x86_64-linux"; crossSystem = "x86_64-windows"; };
-    };
-  in table.${system};
-  default = getArchDefault builtins.currentSystem;
-  makeScripts = cluster: let
-    getScript = name: {
-      x86_64-linux = (getArchDefault "x86_64-linux").scripts.${cluster}.${name};
-      x86_64-darwin = (getArchDefault "x86_64-darwin").scripts.${cluster}.${name};
-    };
-  in {
-    node = getScript "node";
-  };
-  # TODO: add docker images
-  #wrapDockerImage = cluster: let
-  #  images = (getArchDefault "x86_64-linux").dockerImages;
-  #  wrapImage = image: pkgs.runCommand "${image.name}-hydra" {} ''
-  #    mkdir -pv $out/nix-support/
-  #    cat <<EOF > $out/nix-support/hydra-build-products
-  #    file dockerimage ${image}
-  #    EOF
-  #  '';
-  makeRelease = cluster: {
-    name = cluster;
-    value = {
-      scripts = makeScripts cluster;
-      #dockerImage = wrapDockerImage cluster;
-    };
-  };
-  extraBuilds = let
-    # only build nixos tests for linux
-    default = getArchDefault "x86_64-linux";
-  in {
-    inherit nixosTests;
-  } // (builtins.listToAttrs (map makeRelease [
-    "mainnet"
-    "staging"
-    "shelley_staging_short"
-    "shelley_staging"
-    "testnet"
-  ]));
-
   testsSupportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
   # Recurse through an attrset, returning all test derivations in a list.
   collectTests' = ds: filter (d: elem d.system testsSupportedSystems) (collect isDerivation ds);
@@ -106,16 +60,11 @@ let
     "${mingwW64.config}" = mapTestOnCross mingwW64 (packagePlatformsCross project);
     # TODO: fix broken evals
     #musl64 = mapTestOnCross musl64 (packagePlatformsCross project);
-  } // extraBuilds // (mkRequiredJob (
+  } // (mkRequiredJob (
       collectTests jobs.native.checks ++
       collectTests jobs.native.benchmarks ++ [
-      jobs.native.cardano-node.x86_64-darwin
-      jobs.native.cardano-node.x86_64-linux
-      (map (cluster: jobs.${cluster}.scripts.node.x86_64-linux) [ "mainnet" "testnet" "staging" ])
-      # windows cross compilation targets
-      #jobs.x86_64-pc-mingw32.cardano-node.x86_64-linux
-
-      jobs.nixosTests.chairmansCluster.x86_64-linux
+      #jobs.native.cardano-ledger.x86_64-darwin
+      #jobs.native.cardano-ledger.x86_64-linux
     ]));
 
 in jobs
